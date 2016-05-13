acefa9c1816fb38fe1225f6f385556c60255ef2c
Shi Fan <sf2632@nyu.edu>
2016-05-13 19:09:34 -0400

    minor fix

--- analysis/trend_analysis_month.py
+++ analysis/trend_analysis_month.py
@@ -5,7 +5,6 @@ import pandas as pd, numpy as np, geopandas as gpd
 import datetime
 from scipy.stats.stats import pearsonr
 import matplotlib.pyplot as plt
-get_ipython().magic(u'matplotlib inline')
 import seaborn as sns
 sns.set_style("white", {'ytick.major.size': 10.0})
 sns.set_context("poster", font_scale=1.1)
@@ -42,7 +41,7 @@ plt.title('Correlation Histogram between Medallion and Uber Pickups in all Taxi 
 plt.hist([i[1] for i in corr_list])
 plt.savefig('../plots/correlation_histogram.png')
 
-zones = gpd.read_file('../data/taxi_zones.geojson')
+zones = gpd.read_file('../data/geo/taxi_zones.geojson')
 corr_by_zones = pd.DataFrame(corr_list, columns=['zone', 'corr_by_mon', 'p_val']).drop('p_val', axis=1)
 corr_by_zones = pd.merge(zones, corr_by_zones, on='zone')
 corr_by_zones.to_csv('../data/geo/correlation_by_zones.csv', index=False)
